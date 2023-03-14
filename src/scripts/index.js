99c9f146ab15ae2c0a2aa4716de1ed8a526952b5
Paul Rodriguez <paul.rodriguez03@gmail.com>
2023-03-13 21:49:36 -0400

    temp change
    working on temp change

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,13 @@ const locationSubmitBtn = document.querySelector('.location-submit');
 const locations = document.querySelector('.location-input');
 const mainValue = document.querySelector('.main-value');
 const speedValue = document.querySelector('.speed-value');
+const minValue = document.querySelector('.min-value');
+const maxValue = document.querySelector('.max-value');
+const humidityValue = document.querySelector('.humidity-value');
+const pressureValue = document.querySelector('.pressure-value');
+const locationValue = document.querySelector('.location-heading');
+const arrowImg = document.querySelector('.arrow-img');
+const cardTransition = document.querySelector('.weather-cards');
 
 locationSubmitBtn.addEventListener('click', (event) => {
   weatherLookUp();
@@ -20,15 +27,22 @@ async function weatherLookUp() {
       console.log(response);
       mainValueData(
         ferenhitConverter(response.main.temp),
-        windSpeedConverter(response.wind.speed)
+        windSpeedConverter(response.wind.speed),
+        ferenhitConverter(response.main.temp_min),
+        ferenhitConverter(response.main.temp_max),
+        response.main.humidity,
+        response.main.pressure,
+        locations.value,
+        response.wind.deg
       );
+      appTransitions();
     });
 }
 
 function ferenhitConverter(kelvin) {
   const converter = ((kelvin - 273.15) * 9) / 5 + 32;
   let ferenhitValue = converter.toFixed(2);
-  return ferenhitValue;
+  return Number(ferenhitValue);
 }
 function windSpeedConverter(windspeed) {
   const converter = windspeed * 2.237;
@@ -36,8 +50,26 @@ function windSpeedConverter(windspeed) {
   console.log(windspeedValue);
   return Number(windspeedValue);
 }
-
-function mainValueData(mainTemp, speedValue) {
-  mainValue.textContent = `${Number(mainTemp)}°`;
-  speedValue.textContent = `${speedValue} mph`;
+function appTransitions() {
+  cardTransition.style.opacity = 1;
+  cardTransition.style.transform = 'translateY(0)';
+}
+function mainValueData(
+  mainTemp,
+  speedVal,
+  minTemp,
+  maxTemp,
+  humidityVal,
+  pressureVal,
+  locationVal,
+  directionVal
+) {
+  mainValue.textContent = `${mainTemp}°`;
+  speedValue.textContent = `${speedVal} mph`;
+  minValue.textContent = `${minTemp}°`;
+  maxValue.textContent = `${maxTemp}°`;
+  humidityValue.textContent = `${humidityVal}%`;
+  pressureValue.textContent = `${pressureVal} hPa`;
+  locationValue.textContent = locationVal;
+  arrowImg.style.transform = `rotate(${directionVal}deg)`;
 }
